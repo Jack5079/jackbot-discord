273b563d2bc9c635da5df019e6d07d1763d74ef2
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-17 15:56:33 -0500

    Type defs

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
-import { Client } from 'discord.js';
+import { Client, Message } from 'discord.js';
 interface Commands {
-    [key: string]: Function;
+    [key: string]: (message: Message, args: string[], bot: Bot) => any;
 }
 declare class Bot extends Client {
     commands: Commands;
     constructor(commands?: Commands, options?: {
         prefix: string;
     });
-    add(name: string | Commands, func?: Function): void | Function;
+    add(name: string | Commands, func?: (message: Message, args: string[], bot: Bot) => any): void | Function;
     remove(name: string | Array<string>): void;
     get(name: string): Function;
 }
