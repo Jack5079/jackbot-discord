099f702981481e30cece34fd165ff93a31035a4e
Jack <29169102+Jack5079@users.noreply.github.com>
2020-03-05 14:08:07 -0500

    update type def

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
 import { Client, Message } from 'discord.js';
+declare type Command = (message: Message, args: string[], bot: Bot) => any;
 interface Commands {
-    [key: string]: (message: Message, args: string[], bot: Bot) => any;
+    [key: string]: Command;
 }
 declare class Bot extends Client {
     commands: Commands;
     constructor(commands?: Commands, options?: {
         prefix: string;
     });
-    add(name: string | Commands, func?: (message: Message, args: string[], bot: Bot) => any): void | Function;
+    add(name: string | Commands, func?: Command): void | Function;
     remove(name: string | Array<string>): void;
     get(name: string): Function;
 }
-export { Message, Commands, Bot };
+export { Message, Commands, Command, Bot };
